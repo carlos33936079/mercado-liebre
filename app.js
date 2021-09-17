71ea4c12498861fb5d8765f0709bda0bbe6f0fdd
carlos33936079 <c.l.08.cl@gmail.com>
2021-09-16 22:19:53 -0300

    subir 2

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ app.use(express.static(publicPath));
 
 app.listen(port,()=>console.log("servidor en linea http://localhost:3030"));
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
 });
 
 
